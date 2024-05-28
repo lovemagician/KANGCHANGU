49311923d3a0bba9596d05be139d0d605b1e72e6
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-28 18:54:04 +0900

    💄add icon

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,15 +1,19 @@
+import "./Footer.css";
+
 function Footer() {
   return (
     <>
-      <a href="https://www.instagram.com/rkdcksrn/" target="_blank">
-        instagram
-      </a>
-      <a href="https://soundcloud.com/rkdcksrn" target="_blank">
-        SOUNDCLOUD
-      </a>
-      <a href="https://www.youtube.com/@rkdcksrn" target="_blank">
-        YouTube
-      </a>
+      <div className="icons">
+        <a href="https://soundcloud.com/rkdcksrn" target="_blank">
+          <img src="/images/soundcloud_icon.svg" alt="soundcloud" />
+        </a>
+        <a href="https://www.youtube.com/@rkdcksrn" target="_blank">
+          <img src="/images/YouTube_icon.svg" alt="youtube" />
+        </a>
+        <a href="https://www.instagram.com/rkdcksrn/" target="_blank">
+          <img src="/images/Instagram_icon.png" alt="instagram" />
+        </a>
+      </div>
     </>
   );
 }
