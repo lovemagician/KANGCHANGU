fdbcb9c907caa9af71b0bb481e5f9d3fdf891175
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-28 20:10:52 +0900

    🚧 fix Youtube link

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -11,6 +11,26 @@ function Youtube() {
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
       ></iframe>
+      <iframe
+        width="100%"
+        height="315"
+        src="https://www.youtube.com/embed/4lk5gxdGHGA?si=P_445QeAQ2ISs4NU"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+      <iframe
+        width="100%"
+        height="315"
+        src="https://www.youtube.com/embed/tedM3IHYJIo?si=vdn6nyGvmzGAzBUD"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
     </>
   );
 }
