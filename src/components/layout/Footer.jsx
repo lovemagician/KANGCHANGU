d1a96fd67a289d5d43cfc39e3edb896e7666874f
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-28 17:26:18 +0900

    ✨ add Footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <>
+      <a href="https://www.instagram.com/rkdcksrn/" target="_blank">
+        instagram
+      </a>
+      <a href="https://soundcloud.com/rkdcksrn" target="_blank">
+        SOUNDCLOUD
+      </a>
+      <a href="https://www.youtube.com/@rkdcksrn" target="_blank">
+        YouTube
+      </a>
+    </>
+  );
+}
+
+export default Footer;
