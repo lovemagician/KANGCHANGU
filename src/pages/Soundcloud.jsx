bb319f2861dda86bac6bd4cfba1d910b37adcd80
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-28 16:35:08 +0900

    🚧 change player style

--- src/pages/Soundcloud.jsx
+++ src/pages/Soundcloud.jsx
@@ -3,25 +3,12 @@ function Soundcloud() {
     <>
       <iframe
         width="100%"
-        height="166"
+        height="300"
         scrolling="no"
         frameBorder="no"
         allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1729434795&color=%23d0e3e9&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1729434795&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
       ></iframe>
-      <div
-        style={{
-          fontSize: "10px",
-          color: "#cccccc",
-          lineBreak: "anywhere",
-          wordBreak: "normal",
-          overflow: "hidden",
-          whiteSpace: "nowrap",
-          textOverflow: "ellipsis",
-          fontFamily: "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
-          fontWeight: "100",
-        }}
-      ></div>
     </>
   );
 }
