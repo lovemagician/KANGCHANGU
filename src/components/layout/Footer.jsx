534aedffe09ee85123c03093035083e0c6019f5c
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-28 19:04:53 +0900

    💄 add Footer.css

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,19 +2,17 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <>
-      <div className="icons">
-        <a href="https://soundcloud.com/rkdcksrn" target="_blank">
-          <img src="/images/soundcloud_icon.svg" alt="soundcloud" />
-        </a>
-        <a href="https://www.youtube.com/@rkdcksrn" target="_blank">
-          <img src="/images/YouTube_icon.svg" alt="youtube" />
-        </a>
-        <a href="https://www.instagram.com/rkdcksrn/" target="_blank">
-          <img src="/images/Instagram_icon.png" alt="instagram" />
-        </a>
-      </div>
-    </>
+    <div className="icons">
+      <a href="https://soundcloud.com/rkdcksrn" target="_blank">
+        <img src="/images/soundcloud_icon.svg" alt="soundcloud" />
+      </a>
+      <a href="https://www.youtube.com/@rkdcksrn" target="_blank">
+        <img src="/images/YouTube_icon.svg" alt="youtube" />
+      </a>
+      <a href="https://www.instagram.com/rkdcksrn/" target="_blank">
+        <img src="/images/Instagram_icon.png" alt="instagram" />
+      </a>
+    </div>
   );
 }
 
