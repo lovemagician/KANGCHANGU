9193093a20f08a4ca6dd47081425e0110a72a573
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-28 17:28:09 +0900

    ✨ add Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "@components/layout/Footer";
+import Navbar from "@components/layout/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <div>HOME</div>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
