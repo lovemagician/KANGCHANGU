f1f2cc4b70eb5030d8abd13800c77058c2f8451c
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-28 17:25:53 +0900

    🚚 moved from pages to components

---
+++ src/components/Youtube.jsx
@@ -0,0 +1,18 @@
+function Youtube() {
+  return (
+    <>
+      <iframe
+        width="100%"
+        height="315"
+        src="https://www.youtube.com/embed/-5sk9LC2eQE?si=FDnqXyJZKtZ-uwH2"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </>
+  );
+}
+
+export default Youtube;
