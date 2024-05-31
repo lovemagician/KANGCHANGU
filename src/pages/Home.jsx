0f30cf700f95dadb98e7f1cb9621d838153c3720
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-31 14:51:11 +0900

    🚧 fix home (change cover)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,9 @@ function Home() {
     <>
       <Navbar />
       <div className="main">
-        <img className="mainImage" src="/images/KANGCHANGU.jpg" alt="강찬구" />
-        <span>안녕하세요!</span>
+        <img className="mainImageBefore" src="/images/cover_before.jpg" alt="메인 이미지" />
+        <img className="mainImageAfter" src="/images/cover_after.jpg" alt="메인 이미지" />
+        <span></span>
       </div>
       <Footer />
     </>
