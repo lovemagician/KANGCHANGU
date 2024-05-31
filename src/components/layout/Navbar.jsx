ca72dd45ba8c0618e5e88941a35d4fbc586a6cf5
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-31 21:09:28 +0900

    🚧web accessibility

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,9 +4,15 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="navbar">
-      <Link to="/">HOME</Link>
-      <Link to="/music">MUSIC</Link>
-      <Link to="/picture">PICTURE</Link>
+      <Link className="navbar_list" to="/">
+        HOME
+      </Link>
+      <Link className="navbar_list" to="/music">
+        MUSIC
+      </Link>
+      <Link className="navbar_list" to="/picture">
+        PICTURE
+      </Link>
     </div>
   );
 }
