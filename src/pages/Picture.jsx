c790c2fd120bc8e600da7dbabbef68e2ce387c58
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-31 21:10:45 +0900

    🚧 change images

--- src/pages/Picture.jsx
+++ src/pages/Picture.jsx
@@ -12,28 +12,37 @@ function Picture() {
             <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/KANGCHANGU.webp" alt="사진" />
           </div>
           <div>
-            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/KANGCHANGU.webp" alt="사진" />
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/photo_lake.webp" alt="사진" />
           </div>
           <div>
-            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/KANGCHANGU.webp" alt="사진" />
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/photo_kancho.webp" alt="사진" />
           </div>
           <div>
-            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/KANGCHANGU.webp" alt="사진" />
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/photo_sunny.webp" alt="사진" />
           </div>
           <div>
-            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/KANGCHANGU.webp" alt="사진" />
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/drawing_universe.webp" alt="사진" />
           </div>
           <div>
-            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/KANGCHANGU.webp" alt="사진" />
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/photo_moonlight.webp" alt="사진" />
           </div>
           <div>
-            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/KANGCHANGU.webp" alt="사진" />
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/drawing_turtle.webp" alt="사진" />
           </div>
           <div>
-            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/KANGCHANGU.webp" alt="사진" />
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/drawing_city.webp" alt="사진" />
           </div>
           <div>
-            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/KANGCHANGU.webp" alt="사진" />
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/drawing_swimmingpool.webp" alt="사진" />
+          </div>
+          <div>
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/drawing_hurdle.webp" alt="사진" />
+          </div>
+          <div>
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/drawing_paperboat.webp" alt="사진" />
+          </div>
+          <div>
+            <img onClick={(e) => window.open(e.target.src)} width="100%" src="/images/drawing_hill.webp" alt="사진" />
           </div>
         </div>
       </div>
