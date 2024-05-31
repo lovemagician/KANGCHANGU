6166a33484458db215da3b92583d42a39312b578
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-31 21:10:19 +0900

    🚧 add typing effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,37 @@
 import "./Home.css";
 import Footer from "@components/layout/Footer";
 import Navbar from "@components/layout/Navbar";
+import { useEffect } from "react";
 
 function Home() {
+  useEffect(() => {
+    const content = "안녕하세요? 방문해 주셔서 감사합니다.";
+    const text = document.querySelector(".hello");
+    let index = 0;
+
+    function typing() {
+      if (text) {
+        text.textContent += content[index++];
+        if (index >= content.length) {
+          clearInterval(intervalId);
+        }
+      }
+    }
+
+    const intervalId = setInterval(typing, 150);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <>
       <Navbar />
       <div className="main">
-        <img className="mainImageBefore" src="/images/cover_before.webp" alt="메인 이미지" />
-        <img className="mainImageAfter" src="/images/cover_after.webp" alt="메인 이미지" />
-        <span></span>
+        <div className="main_wrapper">
+          <img className="mainImageBefore" src="/images/cover_before.webp" alt="메인 이미지" />
+          <img className="mainImageAfter" src="/images/cover_after.webp" alt="메인 이미지" />
+          <p className="hello"></p>
+        </div>
       </div>
       <Footer />
     </>
