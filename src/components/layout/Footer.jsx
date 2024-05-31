cf1743c4221dcfafb9ff24aee053ea4f935c3109
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-31 23:23:25 +0900

    💄 fix footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,16 +4,18 @@ function Footer() {
   return (
     <>
       <footer className="footer">
-        <span>KANG CHAN GU</span>
-        <a href="https://www.youtube.com/@rkdcksrn" target="_blank">
-          <img src="/images/YouTube_icon.svg" alt="youtube" />
-        </a>
-        <a href="https://soundcloud.com/rkdcksrn" target="_blank">
-          <img src="/images/soundcloud_icon.svg" alt="soundcloud" />
-        </a>
-        <a href="https://www.instagram.com/rkdcksrn/" target="_blank">
-          <img src="/images/Instagram_icon.webp" alt="instagram" />
-        </a>
+        <div className="footer_wrapper">
+          <p className="footer_name">KANG CHAN GU</p>
+          <a className="footer_icon" href="https://www.youtube.com/@rkdcksrn" target="_blank">
+            <img src="/images/YouTube_icon.svg" alt="youtube" />
+          </a>
+          <a className="footer_icon" href="https://soundcloud.com/rkdcksrn" target="_blank">
+            <img src="/images/soundcloud_icon.svg" alt="soundcloud" />
+          </a>
+          <a className="footer_icon" href="https://www.instagram.com/rkdcksrn/" target="_blank">
+            <img src="/images/Instagram_icon.webp" alt="instagram" />
+          </a>
+        </div>
       </footer>
     </>
   );
