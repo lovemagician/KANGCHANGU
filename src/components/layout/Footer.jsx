a37120007bf05ed0978ef49e30f8d6bd9a1b98b8
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-05-31 15:08:29 +0900

    💄 fix home main css

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,17 +2,20 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="icons">
-      <a href="https://soundcloud.com/rkdcksrn" target="_blank">
-        <img src="/images/soundcloud_icon.svg" alt="soundcloud" />
-      </a>
-      <a href="https://www.youtube.com/@rkdcksrn" target="_blank">
-        <img src="/images/YouTube_icon.svg" alt="youtube" />
-      </a>
-      <a href="https://www.instagram.com/rkdcksrn/" target="_blank">
-        <img src="/images/Instagram_icon.png" alt="instagram" />
-      </a>
-    </footer>
+    <>
+      <footer className="footer">
+        <span>KANG CHAN GU</span>
+        <a href="https://www.youtube.com/@rkdcksrn" target="_blank">
+          <img src="/images/YouTube_icon.svg" alt="youtube" />
+        </a>
+        <a href="https://soundcloud.com/rkdcksrn" target="_blank">
+          <img src="/images/soundcloud_icon.svg" alt="soundcloud" />
+        </a>
+        <a href="https://www.instagram.com/rkdcksrn/" target="_blank">
+          <img src="/images/Instagram_icon.png" alt="instagram" />
+        </a>
+      </footer>
+    </>
   );
 }
 
